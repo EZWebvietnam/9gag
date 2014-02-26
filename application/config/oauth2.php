8d57c30201a545c123a63a86248e89d3da63320c
Nguyen Truong Giang <nguyentruonggiang91@gmail.com>
2014-02-26 10:39:27 +0700

    add oauth2 for facebook and google login

---
+++ application/config/oauth2.php
@@ -0,0 +1,23 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Facebook API
+|--------------------------------------------------------------------------
+*/
+$config['facebook_id'] = '1450250688543704';
+$config['facebook_secret'] = '8d12d11ef3524c996c6f656b0cb833e9';
+
+
+/*
+|--------------------------------------------------------------------------
+| Google API
+|--------------------------------------------------------------------------
+*/
+$config['google_id'] = '957752117319-navphphkvrojmn5hi44s2b5eur14nsop.apps.googleusercontent.com';
+$config['google_secret'] = 'a-zolW-tXBD88mWlc1VByO2K';
+
+
+
+/* End of file oauth2.php */
+/* Location: ./application/config/oauth2.php */
--- application/modules/home/controllers/home.php
+++ application/modules/home/controllers/home.php
@@ -45,7 +45,7 @@ class Home extends MY_Controller
     }
     public function google_signup()
     {
-        require_once 'openid.php';
+        /*require_once 'openid.php';
         $openid = new LightOpenID("localhost");
         if($openid->mode)
         {
@@ -75,6 +75,47 @@ class Home extends MY_Controller
         else
         {
             echo "Go to the login page to logged in";
+        }*/
+        $this->load->library('session');
+        $this->load->helper('url_helper');
+        $this->load->library('oauth2/OAuth2');
+		$this->load->config('oauth2', TRUE);
+        $provider_name ='google';
+        $provider = $this->oauth2->provider($provider_name, array(
+            'id' => $this->config->item($provider_name.'_id', 'oauth2'),
+            'secret' => $this->config->item($provider_name.'_secret', 'oauth2'),
+        ));
+
+
+        if ( ! $this->input->get('code'))
+        {
+            // By sending no options it'll come back here
+            $provider->authorize();
+        }
+        else
+        {
+            // Howzit?
+            try
+            {
+                //$token = $provider->access($_GET['code']);
+                $token = $provider->access($this->input->get('code'));
+
+                $user = $provider->get_user_info($token);
+
+                // Here you should use this information to A) look for a user B) help a new user sign up with existing data.
+                // If you store it all in a cookie and redirect to a registration page this is crazy-simple.
+                //echo "<pre>Tokens: ";
+                //var_dump($token);
+
+                echo "\n\nUser Info: ";
+                var_dump($user);
+            }
+
+            catch (OAuth2_Exception $e)
+            {
+                show_error('That didnt work: '.$e);
+            }
+
         }
     }
     public function facebook()
@@ -99,7 +140,7 @@ class Home extends MY_Controller
             echo $auth_url;exit;
 			$this->data['result'] =  '<a href="'.$auth_url.'">'.$auth_url.'</a>';	
 		}*/
-		$fb_config = array(
+		/*$fb_config = array(
 			'appId'  => '1450250688543704',
 			'secret' => '8d12d11ef3524c996c6f656b0cb833e9'
 		);
@@ -123,6 +164,91 @@ class Home extends MY_Controller
         } else {
             $data['login_url'] = $this->facebook
                 ->getLoginUrl();
+        }*/
+        $this->load->library('session');
+        $this->load->helper('url_helper');
+        $this->load->library('oauth2/OAuth2');
+		$this->load->config('oauth2', TRUE);
+        $provider_name ='facebook';
+        $provider = $this->oauth2->provider($provider_name, array(
+            'id' => $this->config->item($provider_name.'_id', 'oauth2'),
+            'secret' => $this->config->item($provider_name.'_secret', 'oauth2'),
+        ));
+
+
+        if ( ! $this->input->get('code'))
+        {
+            // By sending no options it'll come back here
+            $provider->authorize();
+        }
+        else
+        {
+            // Howzit?
+            try
+            {
+                //$token = $provider->access($_GET['code']);
+                $token = $provider->access($this->input->get('code'));
+
+                $user = $provider->get_user_info($token);
+
+                // Here you should use this information to A) look for a user B) help a new user sign up with existing data.
+                // If you store it all in a cookie and redirect to a registration page this is crazy-simple.
+                //echo "<pre>Tokens: ";
+                //var_dump($token);
+
+                echo "\n\nUser Info: ";
+                var_dump($user);
+            }
+
+            catch (OAuth2_Exception $e)
+            {
+                show_error('That didnt work: '.$e);
+            }
+
+        }
+    }
+     public function session($provider_name)
+    {
+        $this->load->library('session');
+        $this->load->helper('url_helper');
+        $this->load->library('oauth2/OAuth2');
+		$this->load->config('oauth2', TRUE);
+
+        $provider = $this->oauth2->provider($provider_name, array(
+            'id' => $this->config->item($provider_name.'_id', 'oauth2'),
+            'secret' => $this->config->item($provider_name.'_secret', 'oauth2'),
+        ));
+
+
+        if ( ! $this->input->get('code'))
+        {
+            // By sending no options it'll come back here
+            $provider->authorize();
+        }
+        else
+        {
+            // Howzit?
+            try
+            {
+                //$token = $provider->access($_GET['code']);
+                $token = $provider->access($this->input->get('code'));
+
+                $user = $provider->get_user_info($token);
+
+                // Here you should use this information to A) look for a user B) help a new user sign up with existing data.
+                // If you store it all in a cookie and redirect to a registration page this is crazy-simple.
+                //echo "<pre>Tokens: ";
+                //var_dump($token);
+
+                //echo "\n\nUser Info: ";
+                //var_dump($user);
+            }
+
+            catch (OAuth2_Exception $e)
+            {
+                show_error('That didnt work: '.$e);
+            }
+
         }
     }
 }
